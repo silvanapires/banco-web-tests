4a4da94da1b55cd6270a33298fa4ee68f24a3cf0
Silvana <silpiressilva@gmail.com>
2025-11-24 23:07:31 -0300

    adicionando um segundo teste.

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -7,9 +7,23 @@ describe('Login', () => {
     //Act
     cy.get('#username').click().type('julio.lima')
     cy.get('#senha').click().type('123456')
-    cy.get('#login-section > .btn').click()
+    cy.contains('button', 'Entrar').click()
 
     //Assert
     cy.contains('h4', 'Realizar Transferência').should('be.visible')
+  })
+
+  it('Login com dados inválidos não deve permitir a entrada no sistema', () => {
+
+    //Arrange
+    cy.visit('http://localhost:4000')
+
+    //Act
+    cy.get('#username').click().type('julio.lima')
+    cy.get('#senha').click().type('15689')
+    cy.contains('button', 'Entrar').click()
+
+    //Assert
+    cy.get('.toast').should('have.text', 'Erro no Login.Tente novamente.')
   })
 })
