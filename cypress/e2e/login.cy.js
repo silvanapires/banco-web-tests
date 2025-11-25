8ad4963a4385d624c6eed76e0edabbe96bb5f27e
Silvana <silpiressilva@gmail.com>
2025-11-25 00:22:36 -0300

    adicionando screenshot.

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,9 +1,10 @@
 describe('Login', () => {
-  it('Login com dados válidos deve permitir a entrada no sistema', () => {
-
-    //Arrange
+beforeEach(() => {
     cy.visit('http://localhost:4000')
+    cy.screenshot('apos-visitar-a-pagina-de-login-do-sistema')
+  })
 
+  it('Login com dados válidos deve permitir a entrada no sistema', () => {
     //Act
     cy.get('#username').click().type('julio.lima')
     cy.get('#senha').click().type('123456')
@@ -13,17 +14,14 @@ describe('Login', () => {
     cy.contains('h4', 'Realizar Transferência').should('be.visible')
   })
 
-  it('Login com dados inválidos não deve permitir a entrada no sistema', () => {
-
-    //Arrange
-    cy.visit('http://localhost:4000')
-
+  it.only('Login com dados inválidos não deve permitir a entrada no sistema', () => {
     //Act
     cy.get('#username').click().type('julio.lima')
     cy.get('#senha').click().type('15689')
+    cy.screenshot('apos-preencher-os-dados-de-login')
     cy.contains('button', 'Entrar').click()
 
     //Assert
-    cy.get('.toast').should('have.text', 'Erro no Login.Tente novamente.')
+    cy.get('.toast').should('have.text', 'Erro no login. Tente novamente.')
   })
 })
